0608490d8e46a0d19e7bc2f110c70645e32fa258
gentylovePY <50623329+gentylovePY@users.noreply.github.com>
2020-06-24 00:38:05 +0300

    Revert "fuulll"
    This reverts commit db73b58a4a534179934b0b304b897810a25fb023.

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
 TOKEN = '1131539159:AAFMULTqDs_33Hro19JMCOq5HCwfbgXcKAg'
 ADMIN_ID = 280588961
 help_text = "Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!"
-MONGO_LINK="mongodb+srv://Roma:<password>@cluster0-od6v8.mongodb.net/<dbname>?retryWrites=true&w=majority"
-MoNGO_DB = 'tm_bot'
